6a3e925c56a437a571741789771ff22fa92348d1
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2024-12-25 19:14:03 +0530

    Time: 0 ms (100%), Space: 8 MB (9.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && (n & (n-1))==0;
+        //powers of two are:      1-0001,  2-0010, 4-0100, 8-1000
+        //.              (n-1)--->0-0000,  1-0001, 3-0011, 7-0111
+        //               n&(n-1)->0000. ,    0000,  0000,  0000
+        return n>0 && (n & (n-1))==0;  
     }
 };
