6b41b8e11bd94000dc213d1247d00dfcaa9140db
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-02-01 10:42:05 +0530

    Time: 1 ms (7.56%), Space: 27.9 MB (2.79%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -4,7 +4,7 @@ public:
        int n=nums.size(); 
        bool par=true;
        for(int i=1;i<n;i++){
-         if((nums[i]%2!=0 && nums[i-1]%2!=0) ||(nums[i]%2==0 && nums[i-1]%2==0)){
+         if(nums[i]%2!=0 == nums[i-1]%2!=0){
             par=false;
          }
          if(!par){
