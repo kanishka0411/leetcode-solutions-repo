456c1610e5fb8584d0efc19b1f3ce6a7dc7947f7
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-03-03 19:47:34 +0530

    Time: 8 ms (50.55%), Space: 127.6 MB (96.86%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,74 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n=nums.size();
-        vector<int>one;
-        vector<int>two;
-        vector<int>three;
-        vector<int>ans;
 
-        for(int i=0;i<n;i++){
+        //-------------------------------------Brute--------------------------------------------//
+        // int n=nums.size();
+        // vector<int>one;
+        // vector<int>two;
+        // vector<int>three;
+        // vector<int>ans;
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot){
+        //         one.push_back(nums[i]);
+        //     }
+        //     if(nums[i]==pivot){
+        //         two.push_back(nums[i]);
+        //     }
+        //     if(nums[i]>pivot){
+        //         three.push_back(nums[i]);
+        //     }
+        // }
+        // ans.insert(ans.end(),one.begin(),one.end());
+        // ans.insert(ans.end(),two.begin(),two.end());
+        // ans.insert(ans.end(),three.begin(),three.end());
+
+        //--------------------------------------Optimized iteration-------------------------------//
+        // int n=nums.size();
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+
+        //-------------------------------------------BEST------------------------------------------
+        int n=nums.size();
+        vector<int>ans(n,0);
+        int i=0;
+        int j=n-1;
+        int ansi=0;
+        int ansj=n-1;
+        while(i<n && j>=0){
             if(nums[i]<pivot){
-                one.push_back(nums[i]);
+                ans[ansi]=nums[i];
+                ansi++;
             }
-            if(nums[i]==pivot){
-                two.push_back(nums[i]);
+            if(nums[j]>pivot){
+                ans[ansj]=nums[j];
+                ansj--;
             }
-            if(nums[i]>pivot){
-                three.push_back(nums[i]);
-            }
+            i++;
+            j--;
         }
-        ans.insert(ans.end(),one.begin(),one.end());
-        ans.insert(ans.end(),two.begin(),two.end());
-        ans.insert(ans.end(),three.begin(),three.end());
+
+        while(ansi<=ansj){
+            ans[ansi]=pivot;
+            ansi++;
+        }
         return ans;
     }
 };
