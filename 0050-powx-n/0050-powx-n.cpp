83ee37e22d7d5e79888c631e13fa7b75e48df920
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-03-19 22:34:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.77%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+
+        //-----------------------------------Brute----------------------------------
+        // int temp=n;
+        // if(n<0){
+        //     x=1/x;
+        //     temp*=-1;
+        // }
+        // double ans=1;
+        // for(int i=1;i<=temp;i++){
+        //     ans*=x;
+        // }
+        // return ans;
+
+        //------------------------------------Optimal---------------------------------
         if(n==0){
             return 1;
         }
-        double halfPow=myPow(x,n/2);
-        double halfPowSq=halfPow*halfPow;
-
-        if(n%2!=0){
-            return (n>0)?x*halfPowSq:halfPowSq/x;
+       long long temp=n;
+        if(temp<0){
+            x=1/x;
+            temp=-temp;
+        }
+        if(temp%2==1){
+            return x*myPow(x,temp-1);
         }
 
-        return halfPowSq;
+        return myPow(x*x,temp/2);
     }
 };
