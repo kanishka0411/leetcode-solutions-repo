7a274293f90ae179c7694e9a935e907b70b81cef
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-05-02 12:55:41 +0530

    Time: 43 ms (10.05%), Space: 49.9 MB (5.41%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
+    bool isSafe(int row,int col,vector<string>temp,int n){
+        for(int i=0;i<row;i++){
+            if(temp[i][col]=='Q') return false;
+        }
 
-    bool isSafe(int row,int col,vector<string>&temp,int n){
-       for(int i=0;i<row;i++){
-        if(temp[i][col]=='Q') return false;
-       }
+        int i=row;
+        int j=col;
+        while(i>=0 && j>=0){
+            if(temp[i][j]=='Q') return false;
+            i--; 
+            j--;
+        }
 
-       int i=row-1,j=col-1;
-       while(i>=0 && j>=0){
-        if(temp[i][j]=='Q') return false;
-        i--;
-        j--;
-       }
-       i=row-1,j=col+1;
+        i=row;
+        j=col;
         while(i>=0 && j<n){
-        if(temp[i][j]=='Q') return false;
-        i--;
-        j++;
-       }
-
-       return true;
+            if(temp[i][j]=='Q') return false;
+            i--;
+            j++;
+        }
+        return true;
     }
-    void func(int n,vector<vector<string>>&ans,vector<string>&temp,int row){
-        if(row==n){
+    void func(int n,vector<vector<string>>&ans,vector<string>&temp,int i){
+        if(i==n){
             ans.push_back(temp);
             return;
         }
         for(int col=0;col<n;col++){
-            if(isSafe(row,col,temp,n)){
-                temp[row][col]='Q';
-                func(n,ans,temp,row+1);
-                temp[row][col]='.';
-            }
+           if(isSafe(i,col,temp,n)){
+            temp[i][col]='Q';
+            func(n,ans,temp,i+1);
+            temp[i][col]='.';
+           } 
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        if(n==2 || n==3){
-            return {};
-        }
         vector<vector<string>>ans;
         vector<string>temp(n,string(n,'.'));
         func(n,ans,temp,0);
-        
         return ans;
     }
 };
