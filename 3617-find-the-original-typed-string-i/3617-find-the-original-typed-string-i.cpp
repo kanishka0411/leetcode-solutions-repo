27a20dfe6931fa7f6106aac68f7bc36e1f560403
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-07-01 14:37:14 +0530

    Time: 5 ms (27.17%), Space: 9 MB (29.69%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        //ye pnc ka lg rha hai 
-        //so jo 2 times hai voh 2 time ya 1 time aa skta hai , yani jo 
-        // n times appear horha hai that can come n times , 1 actual given 
-        // then  so it will be org+(count of different nums-1)
-        //ie 1+1+3=5
         int n=word.size(); 
-int count=1;
-for(int i=n-2;i>=0;i--){
-    if(word[i]==word[i+1]){
-        count++;
+        int count=1;
+        for(int i=0;i<n-1;i++){
+            if(word[i]==word[i+1]){
+              count++;
+            }
+        }
+        return count;
     }
-}
-return count;
-        // so their order also matters , we cant get answer by size only 
-        // like eer is different answer and ere is different 
-        //eer will give er eer so answer=2 
-        //but ere only gives one ->ere
-        //because alice eksaath dabaye hue ke permutation legi
-    }
+
 };
 
-/*
-
-*/
