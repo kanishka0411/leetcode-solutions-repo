87088948e95c890c13297a7534abef7d1ebf22c6
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-08-10 06:49:04 +0530

    Time: 0 ms (100%), Space: 18.6 MB (51.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -8,19 +8,18 @@ public:
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
-                    if(j!=0){
-                        matrix[0][j]=0;
-                    }else{
-                        col0=0;
-                    }
+
+                    if(j!=0) matrix[0][j]=0;
+                    else col0=0;
                 }
+                
             }
         }
 
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j]!=0){
-                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
                         matrix[i][j]=0;
                     }
                 }
@@ -37,5 +36,7 @@ public:
                 matrix[i][0]=0;
             }
         }
+
+
     }
 };
