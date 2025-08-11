2dd711d30f4de790df93c0c2ee5f0d02516d9693
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-08-11 07:03:23 +0530

    Time: 4 ms (75.06%), Space: 126 MB (95.72%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
 
-    int binSearch(vector<int>&vi,int start,int end){
+    bool have(int start,int end,vector<int>&bi){
         int st=0;
-        int en=vi.size()-1;
-
+        int en=bi.size()-1;
         while(st<=en){
             int mid=st+(en-st)/2;
-            if(vi[mid]<start){
+            if(bi[mid]<start){
                 st=mid+1;
-            }else if(vi[mid]>end){
+            }else if(bi[mid]>end){
                 en=mid-1;
             }else{
                 return true;
@@ -18,30 +17,27 @@ public:
         return false;
     }
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int m=queries.size();
         int n=nums.size();
+        int m=queries.size();
+        vector<int>bi;
 
-        vector<int>vi;
         for(int i=1;i<n;i++){
-            if(nums[i]%2==nums[i-1]%2){
-                vi.push_back(i);
+            if(nums[i-1]%2==nums[i]%2){
+                bi.push_back(i);
             }
         }
 
         vector<bool>res(m,false);
-
         for(int i=0;i<m;i++){
             int st=queries[i][0];
             int end=queries[i][1];
-            
-            bool haveVI=binSearch(vi,st+1,end);
 
-            if(haveVI==false){
+            bool hv=have(st+1,end,bi);
+            if(hv==false){
                 res[i]=true;
             }
         }
 
         return res;
-
     }
 };
