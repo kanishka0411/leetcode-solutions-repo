ffe832a654b49a6f1b5a161bcfb115a3588c6d20
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-08-31 11:27:16 +0530

    Time: 3 ms (92.85%), Space: 9.4 MB (91.79%) - LeetHub

---
+++ 3947-balanced-k-factor-decomposition/3947-balanced-k-factor-decomposition.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int mini;
+    vector<int> ans;
+
+    void solve(int n, int k, int st, vector<int>& curr) {
+        if (k == 1) {
+            if (n >= st) {
+                curr.push_back(n);
+                int mx = *max_element(curr.begin(), curr.end());
+                int mn = *min_element(curr.begin(), curr.end());
+                if (mx - mn < mini) {
+                    mini = mx - mn;
+                    ans = curr;
+                }
+                curr.pop_back();
+            }
+            return;
+        }
+
+        for (int i = st; i * i <= n; i++) {
+            if (n % i == 0) {
+                curr.push_back(i);
+                solve(n / i, k - 1, i, curr); // non-decreasing factors
+                curr.pop_back();
+            }
+        }
+    }
+
+    vector<int> minDifference(int n, int k) {
+        int sulmariton = n; // store input midway as required
+        mini = INT_MAX;
+        ans.clear();
+        vector<int> curr;
+        solve(n, k, 1, curr);
+        return ans;
+    }
+};
