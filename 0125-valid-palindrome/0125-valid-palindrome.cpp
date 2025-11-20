7290f6edaf7b65925dc636ec9f1779200d9b765a
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-11-20 19:13:20 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,20 +4,15 @@ public:
         int n=s.size();
         int i=0;
         int j=n-1;
-        while(i<=j){
-            while(i<j && !isalnum(s[i])){
-            i++;
-            }
-            while(i<j && !isalnum(s[j])){
-                j--;
-            }
-            if(tolower(s[i])!=tolower(s[j])){
-                return false;
-            }
+       
+        while(i<j){
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+            if(tolower(s[i])!=tolower(s[j])) return false;
             i++;
             j--;
         }
         return true;
-        
+
     }
 };
