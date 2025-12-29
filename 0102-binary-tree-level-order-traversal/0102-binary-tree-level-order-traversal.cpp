6feeea2148d29ab1cf7fc5df79cbce5ac90d2c0a
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-12-29 18:42:49 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.63%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -23,17 +23,14 @@ public:
                 TreeNode *node=q.front();
                 q.pop();
                 temp.push_back(node->val);
-
                 if(node->left!=nullptr){
                     q.push(node->left);
                 }
-
                 if(node->right!=nullptr){
                     q.push(node->right);
                 }
             }
             ans.push_back(temp);
-
         }
         return ans;
     }
