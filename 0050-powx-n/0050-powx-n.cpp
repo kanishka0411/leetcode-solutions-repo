0019963071db3e5d2f57dc978849043fd8789817
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-03-29 16:40:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.18%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-
-        //-----------------------------------Brute----------------------------------
-        // int temp=n;
-        // if(n<0){
-        //     x=1/x;
-        //     temp*=-1;
-        // }
-        // double ans=1;
-        // for(int i=1;i<=temp;i++){
-        //     ans*=x;
-        // }
-        // return ans;
-
-        //------------------------------------Optimal---------------------------------
-        if(n==0){
-            return 1;
-        }
-       long long temp=n;
-        if(temp<0){
+        long long N=n;
+        if(N<0){
             x=1/x;
-            temp=-temp;
+            N=-N;
+            
         }
-        if(temp%2==1){
-            return x*myPow(x,temp-1);
+        double prod=1;
+        while(N>0){
+            if(N%2==1){
+                prod*=x;
+            }
+            x*=x;
+            N/=2;
         }
-
-        return myPow(x*x,temp/2);
+        return prod;
     }
 };
