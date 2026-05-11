83309cd32ead1a92baf4190661ac89e16e892ac5
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-12 00:24:59 +0530

    Time: 3 ms (35.07%), Space: 77.1 MB (73.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,20 +14,20 @@ public:
         ListNode *dummy=new ListNode();
         ListNode *temp=dummy;
         int carry=0;
-        while((l1!=NULL || l2!=NULL) || carry){
+        while(l1!=nullptr || l2!=nullptr || carry){
             int sum=0;
-            if(l1!=NULL){
+            if(l1!=nullptr){
                 sum+=l1->val;
                 l1=l1->next;
             }
-
-            if(l2!= NULL){
+            if(l2!=nullptr){
                 sum+=l2->val;
                 l2=l2->next;
             }
+
             sum+=carry;
             carry=sum/10;
-            ListNode *newNode= new ListNode(sum%10);
+            ListNode *newNode=new ListNode(sum%10);
             temp->next=newNode;
             temp=temp->next;
         }
