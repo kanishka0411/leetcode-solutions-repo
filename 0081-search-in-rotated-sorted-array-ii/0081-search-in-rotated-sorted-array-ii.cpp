cfcdcd990aa97ed3269f8136dffe9c4ecd6cf6e5
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-21 15:56:46 +0530

    Time: 0 ms (100%), Space: 17.9 MB (63.15%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,25 +5,18 @@ public:
         int st=0;
         int end=n-1;
         while(st<=end){
-            int mid=st+(end-st)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            while(st<mid && nums[st]==nums[mid]){
-                st++;
-            }
-            while(end>mid && nums[mid]==nums[end]){
-                end--;
-            }
-
+            int mid=(st+end)/2;
+            while(nums[mid]==nums[st] && st<mid) st++;
+            while(nums[mid]==nums[end] && end>mid) end--;
+            if(nums[mid]==target) return true;
             if(nums[st]<=nums[mid]){
-                if(nums[st]<=target && target<nums[mid]){
+                if(target>=nums[st] && target<=nums[mid]){
                     end=mid-1;
                 }else{
                     st=mid+1;
                 }
             }else{
-                if(nums[mid]<target && target<=nums[end]){
+                if(target>nums[mid] && target<=nums[end]){
                     st=mid+1;
                 }else{
                     end=mid-1;
@@ -31,5 +24,6 @@ public:
             }
         }
         return false;
+
     }
 };
