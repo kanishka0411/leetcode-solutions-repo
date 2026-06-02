3d6ef06c270cdfc081c7fbdc4ff36ba57340f70f
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-06-02 15:03:49 +0530

    Time: 0 ms (100%), Space: 11.5 MB (42.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,61 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.size()<t.size()) return "";
-        int sindex=-1;
-        int minLen=INT_MAX;
         int n=s.size();
-        int l=0;
-        int r=0;
-        int hash[256]={0};
-        for(auto c:t){
+        int sIdx=-1;
+        int minlen=INT_MAX;
+        vector<int>hash(256,0);
+        for(char c:t){
             hash[c]++;
         }
-        int count=0;
+        int l=0;
+        int r=0;
+        int cnt=0;
         while(r<n){
-            if(hash[s[r]]>0) count++;
-            hash[s[r]]--;
-            while(count==t.size()){
-                if(r-l+1<minLen){
-                    minLen=min(minLen,r-l+1);
-                    sindex=l;
-                }
-
-                hash[s[l]]++;
-                if(hash[s[l]]>0){
-                    count--;
-                }
-                l++;
-                
-               
+            if(hash[s[r]]>0){
+                cnt++;
             }
-             r++;
+            hash[s[r]]--;
+            while(cnt==t.size()){
+                if(r-l+1<minlen){
+                  minlen=r-l+1;
+                  sIdx=l;
+                }
+               hash[s[l]]++;
+               if(hash[s[l]]>0){
+                   cnt--;
+                }
+               l++;
+            }
+            r++;
         }
-        return (sindex==-1)?"":s.substr(sindex,minLen);
+        return sIdx==-1.?"":s.substr(sIdx,minlen);
     }
 };
-
-
-
-
-
-
-/*for(int i=0;i<n;i++){
-            int hash[256]={0};
-            for(auto c:t){
-                hash[c]++;
-            }
-            int count=0;
-            for(int j=i;j<n;j++){
-                if(hash[s[j]]>0) count++;
-                hash[s[j]]--;
-                if(count==t.size()){
-                    if(j-i+1<minLen){
-                        minLen=j-i+1;
-                        sindex=i;
-                        
-                    }
-                    break;
-                }
-            }
-        }*/
