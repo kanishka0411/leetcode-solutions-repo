f8fc315279f58c1376b8ef58bb14fdd0cfc3ebdb
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-07-10 02:48:35 +0530

    Time: 11 ms (39.75%), Space: 29.2 MB (90.02%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    bool isValid(int row,int col,int n,int m){
-        if(row<0 || row>=n) return false;
-        if(col<0 || col>=m) return false;
+    bool isValid(int r,int c,int n,int m){
+        if(r<0 || r>=n) return false;
+        if(c<0 || c>=m) return false;
+
         return true;
     }
     int numEnclaves(vector<vector<int>>& grid) {
@@ -10,7 +11,6 @@ public:
         int m=grid[0].size();
         queue<pair<int,int>>q;
         vector<vector<bool>>vis(n,vector<bool>(m,false));
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0 || i==n-1 || j==0 || j==m-1){
@@ -21,32 +21,31 @@ public:
                 }
             }
         }
-        int delrow[4]={-1,0,1,0};
-        int delcol[4]={0,1,0,-1};
+        int r[4]={-1,0,1,0};
+        int c[4]={0,1,0,-1};
         while(!q.empty()){
             auto it=q.front();
+            q.pop();
             int row=it.first;
             int col=it.second;
-            q.pop();
-
             for(int i=0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
-                if(isValid(nrow,ncol,n,m) && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
+                int newrow=row+r[i];
+                int newcol=col+c[i];
+
+                if(isValid(newrow,newcol,n,m) && !vis[newrow][newcol] && grid[newrow][newcol]==1){
+                    vis[newrow][newcol]=true;
+                    q.push({newrow,newcol});
                 }
             }
         }
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
+                if(grid[i][j]==1 && !vis[i][j]){
                     cnt++;
                 }
             }
         }
         return cnt;
-
     }
 };
