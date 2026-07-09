9d49d194b90bd32abc7feba6c988ce86d2b436b4
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-07-10 00:20:37 +0530

    Time: 32 ms (24.96%), Space: 23.7 MB (24.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    bool isValid(int row,int col,int n,int m){
-        if(row<0 || row>=n) return false;
-        if(col<0 || col>=m) return false;
-
+    bool isValid(int r,int c,int n,int m){
+        if(r<0 || r>=n) return false;
+        if(c<0 || c>=m) return false;
         return true;
     }
     void bfs(int i,int j,vector<vector<char>>&grid,vector<vector<bool>>&vis){
-        vis[i][j]=true;
-        queue<pair<int,int>>q;
-        q.push({i,j});
         int n=grid.size();
         int m=grid[0].size();
-        int r[]={-1,0,1,0};
-        int c[]={0,1,0,-1};
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=true;
+        int r[4]={-1,0,1,0};
+        int c[4]={0,1,0,-1};
         while(!q.empty()){
             auto it=q.front();
-            q.pop();
             int row=it.first;
             int col=it.second;
-  
-            for(int k=0;k<4;k++){
-                int newrow=row+r[k];
-                int newcol=col+c[k];
-                if(isValid(newrow,newcol,n,m) && grid[newrow][newcol]=='1' && !vis[newrow][newcol]){
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newrow=row+r[i];
+                int newcol=col+c[i];
+
+                if(isValid(newrow,newcol,n,m) && !vis[newrow][newcol] && grid[newrow][newcol]=='1'){
                     vis[newrow][newcol]=true;
                     q.push({newrow,newcol});
                 }
             }
         }
-        
     }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int cnt=0;
         vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
@@ -45,5 +43,6 @@ public:
             }
         }
         return cnt;
+
     }
 };
