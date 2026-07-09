7c84010c1fde8fbc4742e15d6b1fc9babb29570d
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-07-10 02:08:28 +0530

    Time: 3 ms (13.03%), Space: 18.3 MB (32.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int delrow[4]={-1,0,1,0};
-    int delcol[4]={0,1,0,-1};
     bool isValid(int r,int c,int n,int m){
         if(r<0 || r>=n) return false;
         if(c<0 || c>=m) return false;
         return true;
     }
-    void dfs(int row,int col,vector<vector<int>>&ans,vector<vector<int>>&image,int color,int inColor){
-        ans[row][col]=color;
+    void bfs(vector<vector<int>>&image,int sr,int sc,vector<vector<int>>&ans,int color,int iniColor){
         int n=image.size();
         int m=image[0].size();
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        ans[sr][sc]=color;
+        int r[4]={-1,0,1,0};
+        int c[4]={0,1,0,-1};
+        while(!q.empty()){
+            auto it=q.front();
+            int row=it.first;
+            int col=it.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newrow=row+r[i];
+                int newcol=col+c[i];
 
-            if(isValid(nrow,ncol,n,m) && image[nrow][ncol]==inColor && ans[nrow][ncol]!=color){
-                ans[nrow][ncol]=color;
-                dfs(nrow,ncol,ans,image,color,inColor);
+                if(isValid(newrow,newcol,n,m) && ans[newrow][newcol]!=color && image[newrow][newcol]==iniColor){
+                    ans[newrow][newcol]=color;
+                    q.push({newrow,newcol});
+                }
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int inColor=image[sr][sc];
+        int iniColor=image[sr][sc];
         vector<vector<int>>ans=image;
-        dfs(sr,sc,ans,image,color,inColor);
+        bfs(image,sr,sc,ans,color,iniColor);
         return ans;
     }
 };
