53287c5fafedb2b9c533329221c3a2c01a590f37
Kanishka.. <kanishka0411k@gmail.com>
2026-08-25 00:51:37 +0530

    Time: 0 ms (100%), Space: 22.1 MB (59.92%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>ans;
+        stack<int>st;
+     
         int n=asteroids.size();
-        vector<int>st;
         for(int i=0;i<n;i++){
-            if(asteroids[i]>0) st.push_back(asteroids[i]);
-            else{
-                while(!st.empty() && st.back()>0 && st.back()<abs(asteroids[i])){
-                    st.pop_back();
+            if(asteroids[i]>0){
+                st.push(asteroids[i]);
+            }else{
+                bool alive=true;
+                while(!st.empty()){
+                    int ast=st.top();
+                    if(abs(asteroids[i])==ast){
+                        st.pop();
+                        alive=false;
+                        break;
+                    }else if(ast<0){
+                        st.push(asteroids[i]);
+                        alive=false;
+                        break;
+                    }else if(abs(asteroids[i])<ast){
+                        alive=false;
+                        break;
+                    }else{
+                        alive=true;
+                        st.pop();
+                        continue;
+                    }
                 }
-
-                if(!st.empty() && st.back()==abs(asteroids[i])) st.pop_back();
-                else if(st.empty() || st.back()<0) st.push_back(asteroids[i]);
+                if(alive){
+                    st.push(asteroids[i]);
+                }
             }
         }
-        return st;
+
+        while(!st.empty()){
+            auto it=st.top();
+            st.pop();
+            ans.push_back(it);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
