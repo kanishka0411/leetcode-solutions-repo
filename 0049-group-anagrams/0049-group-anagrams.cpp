7ae1c56d4d5018e7f9ac6c8406c885fc9a68c869
Kanishka.. <kanishka0411k@gmail.com>
2026-08-28 13:16:11 +0530

    Time: 14 ms (74.19%), Space: 26.2 MB (30.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-
-    string generate(string &word){
-        int arr[26]={0};
-        for(char &ch:word){
-            arr[ch-'a']++;
-        }
-        string new_word="";
-        for(int i=0;i<26;i++){
-            int freq=arr[i];
-            if(freq>0){
-                new_word+=string(freq,i+'a');
-                
-            }
-        }
-        return new_word;
-        
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mpp;
         int n=strs.size();
-        unordered_map<string,vector<string>>mpp;;
-        vector<vector<string>>result;
-        for(int i=0;i<n;i++){
-            string word=strs[i];
-            string new_Word=generate(word);
-            mpp[new_Word].push_back(word);
+        for(string s:strs){
+            string key=s;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(s);
+
         }
 
-        for(auto &it:mpp){
-            result.push_back(it.second);
+        for(auto it:mpp){
+            ans.push_back(it.second);
         }
-        return result;
+
+        return ans;
     }
 };
