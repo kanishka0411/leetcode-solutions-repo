54ad40ac6c06285d2880b142d0047f7d2188be06
Kanishka.. <kanishka0411k@gmail.com>
2026-08-29 01:09:50 +0530

    Time: 0 ms (100%), Space: 9.7 MB (49.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(t.size()!=s.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]) return false;
-        }
+       if(s.size()!=t.size()) return false;
+       vector<int>freq(26,0);
+       for(int i=0;i<s.size();i++){
+        freq[s[i]-'a']++;
+        freq[t[i]-'a']--;
+       }
 
-        return true;
+       for(int i=0;i<26;i++){
+         if(freq[i]!=0) return false;
+       }
+
+       return true;
+       
     }
 };
