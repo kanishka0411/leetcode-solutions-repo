3e1d94cc15672a038188b3b243d023da0fbbde43
Kanishka.. <kanishka0411k@gmail.com>
2026-09-09 00:18:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (85.67%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         int n=board.size();
-        int live=0;
         int m=board[0].size();
         vector<int>row={0,-1,0,1,1,-1,-1,1};
         vector<int>col={1,0,-1,0,1,1,-1,-1};
         vector<vector<int>>copy=board;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                for(int k=0;k<row.size();k++){
+                int live=0;
+                for(int k=0;k<8;k++){
                     int r=i+row[k];
                     int c=j+col[k];
                     if(r>=0 && r<n && c>=0 && c<m && copy[r][c]==1) live++;
-                    if(copy[i][j]==1 && live<2) board[i][j]=0;
-                    if(copy[i][j]==1 && live>3) board[i][j]=0;
-                    if(copy[i][j]==0 && live==3) board[i][j]=1;
                 }
+                if(copy[i][j]==1 && live<2) board[i][j]=0;
+                if(copy[i][j]==1 && live>3) board[i][j]=0;
+                if(copy[i][j]==0 && live==3) board[i][j]=1;
+                
             }
         }
     }
