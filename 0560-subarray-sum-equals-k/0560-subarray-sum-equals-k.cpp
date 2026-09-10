5b4683ed625dcc53e1e5b354136ca46126bc29c5
Kanishka.. <kanishka0411k@gmail.com>
2026-09-10 11:06:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        int psum=0;
-        int cnt=0;
         unordered_map<int,int>mpp;
         mpp[0]=1;
+        int cnt=0;
+        int psum=0;
         for(int i=0;i<n;i++){
             psum+=nums[i];
-            cnt+=mpp[psum-k];
+            cnt+= mpp[psum-k]++;
             mpp[psum]++;
         }
         return cnt;
