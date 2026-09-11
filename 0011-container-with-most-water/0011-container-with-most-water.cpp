8c7092ff158f97167780eb70c31986de4793f1ed
Kanishka.. <kanishka0411k@gmail.com>
2026-09-11 22:09:49 +0530

    Time: 0 ms (100%), Space: 62.7 MB (97.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n=height.size();
-        int maxAmount=0;
-        int curr=0;
         int i=0;
         int j=n-1;
-        while(i<j){
-            curr=min(height[i],height[j])*(j-i);
-            maxAmount=max(maxAmount,curr);
+        int maxi=0;
+        while(i<=j){
+            int area=min(height[i],height[j])*(j-i);
+            maxi=max(maxi,area);
             if(height[i]<height[j]) i++;
             else j--;
         }
-        return maxAmount;
-        
+        return maxi;
     }
 };
